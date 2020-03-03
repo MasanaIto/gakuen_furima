36c3259b5b331ea1f3208cfa102f8301040a9827
MasanaIto <z3heng0326@gmail.com>
2020-03-03 22:44:34 +0900

    ログアウトAPIを実装

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -48,4 +48,17 @@ class LoginController extends Controller
     {
         return $user;
     }
+
+
+
+    /**
+     * ログアウト
+     */
+    protected function loggedOut(Request $request)
+    {
+    // セッションを再生成する
+    $request->session()->regenerate();
+
+    return response()->json();
+    }
 }
